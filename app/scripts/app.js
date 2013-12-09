9714f0480637ba954d05263a51a82a488854278b
byEDU <eduardoterradez@gmail.com>
2013-12-09 06:52:10 -0500

    Añadir rectangulos y alertas cuando haces click

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
 /*global define */
 define([], function () {
     'use strict';
-
-    return '\'Allo \'Allo!';
+    $(document).ready(function(){
+    	$('#Sblue').click(function(){
+            alert("Este es el azul");
+        });
+    	$('#Sgreen').click(function(){
+            alert("Este es el verde");
+        });
+    	$('#Sred').click(function(){
+            alert("Este es el rojo");
+        });
+    	$('#Syellow').click(function(){
+            alert("Este es el amarillo");
+        });
+    })
+    return 0;
 });
