422b704de4e19d3ec4c1ac5d526c15ea133fff1e
byEDU <eduardoterradez@gmail.com>
2013-12-11 18:17:43 -0500

    Forma de Rombo, Array que guarda los clics sobre colores

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,31 @@
 /*global define */
 define([], function () {
-    'use strict';
-    $(document).ready(function(){
-    	$('#Sblue').click(function(){
-            alert("Este es el azul");
-        });
-    	$('#Sgreen').click(function(){
-            alert("Este es el verde");
-        });
-    	$('#Sred').click(function(){
-            alert("Este es el rojo");
-        });
-    	$('#Syellow').click(function(){
-            alert("Este es el amarillo");
-        });
-    })
-    return 0;
+	'use strict';
+
+	var myArray = []
+
+	$(document).ready(function() {
+		$(".button").click( function(){
+			var thisId = $(this).attr('id')
+			console.log(thisId)
+
+			if ( thisId === "Sred") {
+				myArray.push (thisId)
+				$(this).css('background-color','#FF6666')
+			} else if ( thisId === "Sblue"){
+				myArray.push (thisId) 
+				$(this).css('background-color','#3366FF')
+			} else if ( thisId === "Syellow"){
+				myArray.push (thisId)
+				$(this).css('background-color','#FFFF99')
+			} else if ( thisId === "Sgreen"){
+				myArray.push (thisId)
+				$(this).css('background-color','#66FF66')
+			} else {
+				console.log("Error")
+			}    
+			console.log (myArray)
+		})
+	})
+	return 0;
 });
